ba2ac8b738152b0897ee40b2822c6292ac2f2971
zRGUAz <rafael.gualdieri.pinheiro@gmail.com>
2024-09-26 22:12:37 -0300

    first commit

---
+++ web-app-performance/Controllers/UsuarioController.cs
@@ -0,0 +1,103 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using Newtonsoft.Json;
+using StackExchange.Redis;
+using web_app_performance.Model;
+
+namespace web_app_performance.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private static ConnectionMultiplexer redis;
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsuario()
+        {
+
+            string key = "getusuario";
+            redis = ConnectionMultiplexer.Connect("localhost:6379");
+            IDatabase db = redis.GetDatabase();
+            await db.KeyExpireAsync(key, TimeSpan.FromSeconds(10)); 
+            string user = await db.StringGetAsync(key);
+
+            if(!string.IsNullOrEmpty(user))
+            {
+                return Ok(user);
+            }
+
+            string connectionString = "Server=localhost;Database=sys;User=root;Password=123";
+            using var connection = new MySqlConnection(connectionString);
+            await connection.OpenAsync();
+            string query = "SELECT id, nome, email FROM usuarios;";
+            var usuarios = await connection.QueryAsync<Usuario>(query);
+            string usuariosJson = JsonConvert.SerializeObject(usuarios);
+            await db.StringSetAsync(key, usuariosJson);
+
+            return Ok(usuarios);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostUsuario([FromBody] Usuario usuario)
+        {
+            string connectionString = "Server=localhost;Database=sys;User=root;Password=123";
+            using var connection = new MySqlConnection(connectionString);
+            await connection.OpenAsync();
+
+            string sql = "INSERT INTO usuarios(nome, email) VALUES(@nome, @email)";
+            await connection.ExecuteAsync(sql, usuario);
+
+            //apaga o cache
+            string key = "getusuario";
+            redis = ConnectionMultiplexer.Connect("localhost:6379");
+            IDatabase db = redis.GetDatabase();
+            await db.KeyDeleteAsync(key);
+
+            return Ok();
+
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutUsuario([FromBody] Usuario usuario)
+        {
+            string connectionString = "Server=localhost;Database=sys;User=root;Password=123";
+            using var connection = new MySqlConnection(connectionString);
+            await connection.OpenAsync();
+
+            string sql = "UPDATE usuarios SET nome = @nome, email = @email WHERE id = @id";
+            await connection.ExecuteAsync(sql, usuario);
+
+            //apaga o cache
+            string key = "getusuario";
+            redis = ConnectionMultiplexer.Connect("localhost:6379");
+            IDatabase db = redis.GetDatabase();
+            await db.KeyDeleteAsync(key);
+
+            return Ok();
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            string connectionString = "Server=localhost;Database=sys;User=root;Password=123";
+            using var connection = new MySqlConnection(connectionString);
+            await connection.OpenAsync();
+
+            string sql = "DELETE FROM usuarios WHERE id = @id";
+            await connection.ExecuteAsync(sql, new { id });
+
+            //apaga o cache
+            string key = "getusuario";
+            redis = ConnectionMultiplexer.Connect("localhost:6379");
+            IDatabase db = redis.GetDatabase();
+            await db.KeyDeleteAsync(key);
+
+            return Ok();
+
+        }
+
+    }
+}
---
+++ web-app-performance/Model/Usuario.cs
@@ -0,0 +1,9 @@
+﻿namespace web_app_performance.Model
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+    }
+}
